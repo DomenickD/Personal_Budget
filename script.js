44aebd1143c90b07085ed76b0783087ee426641e
DomenickD <104775966+DomenickD@users.noreply.github.com>
2022-05-10 14:35:58 -0400

    Add files via upload
    Initial upload of incomplete project.

---
+++ script.js
@@ -0,0 +1,5 @@
+//interactivity with index.html to be here
+//include module-1 to create objects
+const moduleOne = require('./module-1');
+
+const startingAmount = document.getElementById('Paycheck');
